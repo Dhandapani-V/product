f97114f4ae181811018d0c91a9e684db95055a4f
Mounika <mounika@skkpentcorp.com>
2025-06-19 18:10:34 +0530

    product id eror

--- app/productDetail/page.jsx
+++ app/productDetail/page.jsx
@@ -13,7 +13,7 @@ export default function Page({ params }) {
   console.log("Product ID from query:", id);
   return (
     <div>
-      <p>{productId}</p>
+      <p>{id}</p>
       <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
         <ContactHeader />
         <TopBar />
